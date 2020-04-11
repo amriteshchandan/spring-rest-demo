bbdb798c5dcf3f0e52efa611048b8f7b90926828
amriteshchandan <amriteshchandan@yahoo.in>
2020-04-12 01:44:57 +0530

    @PathVariable

--- src/main/java/com/luv2code/demo/rest/StudentRESTController.java
+++ src/main/java/com/luv2code/demo/rest/StudentRESTController.java
@@ -3,6 +3,9 @@ package com.luv2code.demo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +16,23 @@ import com.luv2code.demo.entity.Student;
 @RequestMapping(path = "/api", method = RequestMethod.GET)
 public class StudentRESTController {
 
-	@RequestMapping(path = "/students", method = RequestMethod.GET)
-	public List<Student> getStudents() {
-		List<Student> students = new ArrayList<>();
+	private List<Student> students = new ArrayList<>();
+
+	@PostConstruct
+	public void loadData() {
 		students.add(new Student("Amritesh", "Chandan"));
 		students.add(new Student("Aman", "Raj"));
 		students.add(new Student("Jayant", "Kumar"));
+	}
+
+	@RequestMapping(path = "/students", method = RequestMethod.GET)
+	public List<Student> getStudents() {
 		return students;
 	}
 	
+	@RequestMapping(path = "/students/{studentID}", method = RequestMethod.GET)
+	public Student getStudent(@PathVariable int studentID) {
+		return students.get(studentID);
+	}
+
 }
