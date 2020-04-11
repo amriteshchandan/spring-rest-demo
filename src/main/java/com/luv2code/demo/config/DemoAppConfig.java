d15c6ffe64e47bee9940a1e7b8528056a105613d
amriteshchandan <amriteshchandan@yahoo.in>
2020-04-11 23:05:56 +0530

    Config

--- src/main/java/com/luv2code/demo/config/DemoAppConfig.java
+++ src/main/java/com/luv2code/demo/config/DemoAppConfig.java
@@ -1,5 +1,12 @@
 package com.luv2code.demo.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.luv2code.demo")
 public class DemoAppConfig {
 
 }
